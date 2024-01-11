c1a9761f741a4b031599453daea63cec0c07c79c
sschellhoff <soeren.schellhoff@gmail.com>
2024-01-11 23:37:07 +0100

    fixed return type of compile

--- compiler/app/Compile.hs
+++ compiler/app/Compile.hs
@@ -10,18 +10,24 @@ import Control.Monad.Except (runExceptT)
 import qualified Environment
 import TypeInference
 
-runFileOrFail :: String -> IO (Either FmlError.FmlError (Either FmlError.FmlError ByteCode))
+runFileOrFail :: String -> IO (Either FmlError.FmlError ByteCode)
 runFileOrFail filename = do
     compile filename <$> readFile filename
 
--- TODO return early on first error to result in a singl either
-compile :: String -> String -> Either FmlError.FmlError (Either FmlError.FmlError ByteCode)
+compile :: String -> String -> Either FmlError.FmlError ByteCode
 compile filename content = do
-    ast <- FMLParser.parseContentOrFail filename content
-    return $ evalState (runExceptT (transformAst ast)) Environment.emptyEnv
+    joinEither _compile where
+        _compile = do
+            ast <- FMLParser.parseContentOrFail filename content
+            return $ evalState (runExceptT (transformAst ast)) Environment.emptyEnv
 
 transformAst ::FmlSemanticStep Ast.ParsedProgram ByteCode
 transformAst ast = do
     _ast <- TypeInference.addTypeProgramOrFail ast
     _checkedProgram <- TypeCheck.typeCheckProgramOrFail _ast
     Backend.generate _checkedProgram
+
+joinEither :: Either a (Either a b) -> Either a b
+joinEither (Left err) = Left err
+joinEither (Right (Left err)) = Left err
+joinEither (Right (Right value)) = Right value
